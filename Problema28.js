a21d28db0bb9150bdb38b5f20573edaafd0b3ee6
Denzel Muñoz <a348802@uach.mx>
2023-04-03 17:56:32 -0600

    Programa_28

---
+++ Problema28.js
@@ -0,0 +1,10 @@
+function Moneda() {
+    const resultado = Math.random();
+    if (resultado > 0.5) {
+      console.log("Cara");
+    } else {
+      console.log("Cruz");
+    }
+  }
+  
+  Moneda();
