6bf3b65b5ea063c958d2cac75ec2122f1a76fd21
Denzel Muñoz <a348802@uach.mx>
2023-04-03 19:24:42 -0600

    Programa_32

---
+++ Problema32.js
@@ -0,0 +1,5 @@
+const personas = ['Juan', 'Denzel', 'Samuel', 'Jessica', 'Anahí', 'Marcela', 'Angelica', 'Diego', 'Yoss', 'Paul', 'Luis', 'Paulina', 'Mónica', 'José', 'Valeria', 'Cristian', 'Itsi', 'Marlene', 'Mike', 'Abraham'];
+
+for (let i = 0; i < personas.length; i++) {
+  console.log(personas[i]);
+}
